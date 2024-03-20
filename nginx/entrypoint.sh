bf6773f4c7294a5b342e9510c3db9cc7d5828aa0
Panagiotis <400362+panosru@users.noreply.github.com>
2024-03-20 20:13:40 +0200

    Update nginx with a better configuration

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # Ensure /var/run/nginx (PID directory) exists and is owned by nginx
 mkdir -p /var/run/nginx
 chown nginx:nginx /var/run/nginx
+chmod 755 /var/run/nginx
 
 # Change ownership of the Nginx log files
 chown nginx:nginx /var/log/nginx/access.log /var/log/nginx/error.log
@@ -14,13 +15,14 @@ LOG_ROTATE=${LOG_ROTATE:-1}
 ADDITIONAL_DIRECTIVES=${ADDITIONAL_DIRECTIVES:-""}
 CLIENT_MAX_BODY_SIZE=${CLIENT_MAX_BODY_SIZE:-100m}
 FASTCGI_PASS=${FASTCGI_PASS:-""}
+SERVER_NAME=${SERVER_NAME:-"_"}
 
 # Export variables for envsubst
 export LOG_SIZE LOG_ROTATE ADDITIONAL_DIRECTIVES CLIENT_MAX_BODY_SIZE FASTCGI_PASS
 
 # Apply configurations for logrotate and Nginx
 envsubst '${LOG_SIZE} ${LOG_ROTATE} ${ADDITIONAL_DIRECTIVES}' < /etc/logrotate.d/nginx-logrotate.template > /etc/logrotate.d/nginx
-envsubst '${CLIENT_MAX_BODY_SIZE},${FASTCGI_PASS}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+envsubst '${CLIENT_MAX_BODY_SIZE},${SERVER_NAME},${FASTCGI_PASS}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 # Correct permissions to ensure nginx user can read the configurations
 chown root:root /etc/logrotate.d/nginx /etc/nginx/conf.d/default.conf
@@ -31,8 +33,7 @@ if [ -d /etc/nginx/custom.conf.d ]; then
     CONF_FILES=$(find /etc/nginx/custom.conf.d -type f -name "*.conf")
     if [ -n "$CONF_FILES" ]; then
         for f in $CONF_FILES; do
-            echo "Including custom Nginx configuration: $f"
-            . "$f"
+            echo "Found custom Nginx configuration: $f"
         done
     else
         echo "No custom Nginx configuration files found in /etc/nginx/custom.conf.d"
